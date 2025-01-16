463c4e185bf3209fb630c8b605614722fb34e082
Akmalmp1 <157487756+akmalmp1@users.noreply.github.com>
2025-01-16 21:56:29 +0700

    Perbaikan Repo

--- 220660121208/PHP DAN API/CREATE DATABASE todolist;.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE todolist;
-
-USE todolist;
-
-CREATE TABLE todos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    text VARCHAR(255) NOT NULL,
-    completed BOOLEAN NOT NULL DEFAULT 0
-);
